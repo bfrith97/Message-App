dde1b9ebda4334acabd42ee60aa62014279a5965
bfrith97 <107682768+bfrith97@users.noreply.github.com>
2022-06-18 13:56:37 +0100

    Add files via upload

--- Message/script.js
+++ Message/script.js
@@ -1,4 +1,7 @@
 //MODALsend
+
+let currentUser = "";
+
 const modalBkgrnd = document.querySelector(".modal-background");
 const lgnModal = document.querySelector(".login-modal");
 const buttons = document.querySelectorAll(".login-button");
@@ -8,27 +11,31 @@ const chatActive = document.querySelector(".chatbar-active");
 const chatWindowActive = document.querySelector(".chat-window-active");
 const chatSlctrs = document.querySelector(".chats-selectors");
 
+const initialise = function (user) {
+  currentUser = user;
+  modalBkgrnd.style.display = "none";
+  lgnModal.style.display = "none";
+
+  ctnrUserRight.style.display = "grid";
+  ctnrUserLeft.style.display = "flex";
+
+  chatFirst.classList.add("chatbar-active");
+  msgTop.style.display = "initial";
+  msgContainer.style.display = "flex";
+  chatSlctrs.style.display = "flex";
+
+  msgContent.style.display = "initial";
+
+  btnSend.style.display = "initial";
+};
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("login-button-one")) {
-      modalBkgrnd.style.display = "none";
-      lgnModal.style.display = "none";
-
-      ctnrUser.forEach((ctnr) => {
-        ctnr.style.display = "flex";
-      });
-
-      chatFirst.classList.add("chatbar-active");
-      msgTop.style.display = "initial";
-      msgContainer.style.display = "flex";
-      chatSlctrs.style.display = "flex";
-
-      msgContent.style.display = "initial";
-
-      btnSend.style.display = "initial";
+      initialise(1);
     }
     if (button.classList.contains("login-button-two")) {
-      console.log("button 2");
+      initialise(2);
     }
   });
 });
@@ -38,7 +45,6 @@ buttons.forEach((button) => {
 const msgCreate = document.createElement("div");
 const msgTop = document.querySelector(".message-top");
 const msg = document.querySelectorAll(".message");
-msgCreate.classList.add("message");
 const msgContainer = document.querySelector(".message-write");
 const msgContent = document.querySelector(".message-content");
 const chatWindow = document.querySelectorAll(".chat-window");
@@ -68,7 +74,9 @@ msgContent.addEventListener("keydown", (key) => {
       msgCreate.innerHTML = msgContent.value;
       msgTop.insertAdjacentHTML(
         "beforeend",
-        `<div class="message">${msgContent.value}</div>`
+        `<div class="message-user-${currentUser === 1 ? "one" : "two"}">${
+          msgContent.value
+        }</div>`
       );
       //
     }
@@ -83,7 +91,9 @@ btnSend.addEventListener("click", () => {
     msgCreate.innerHTML = msgContent.value;
     msgTop.insertAdjacentHTML(
       "beforeend",
-      `<div class="message">${msgContent.value}</div>`
+      `<div class="message-user-${currentUser === 1 ? "one" : "two"}">${
+        msgContent.value
+      }</div>`
     );
     msgContent.value = "";
   }
@@ -96,7 +106,10 @@ clearChat.addEventListener("click", () => {
 // USERS
 
 const signOutBtn = document.querySelector(".btn-signout");
-const ctnrUser = document.querySelectorAll(".container-user");
+// const ctnrUser = document.querySelectorAll(".container-user");
+const ctnrUserRight = document.querySelector(".container-user-right");
+const ctnrUserLeft = document.querySelector(".container-user-left");
+const userImg = document.querySelector(".user-image");
 
 // SIGN OUT
 
@@ -117,7 +130,6 @@ signOutBtn.addEventListener("click", () => {
     window.classList.remove("chatbar-active");
   });
 
-  ctnrUser.forEach((ctnr) => {
-    ctnr.style.display = "none";
-  });
+  ctnrUserRight.style.display = "none";
+  ctnrUserLeft.style.display = "none";
 });
